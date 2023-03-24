7739baa0b676147656749e8a5f951b0f68cc61f8
NabilAllouche <nabilallouche49@gmail.com>
2023-03-24 23:26:23 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommandeCreateComponent } from './view/commandes/commande-create/commande-create.component';
+import { CommandeListComponent } from './view/commandes/commande-list/commande-list.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommandeCreateComponent,
+    CommandeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
